23beb71bd855d01e943c3a13fb4944a6a023b557
Martin <martinald467@gmail.com>
2021-02-26 21:33:59 +0100

    added main file

---
+++ cmd/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/SirNoob97/yt-transcripts/client"
+)
+
+func main() {
+	helpFlag := flag.Bool("help", false, "Display help message")
+	flag.Parse()
+	s := client.NewSwitch()
+
+	if *helpFlag || len(os.Args) == 1 {
+		s.Help()
+		return
+	}
+
+	err := s.Switch()
+	if err != nil {
+		fmt.Printf("cmd switch error %v\n", err)
+		os.Exit(2)
+	}
+}
