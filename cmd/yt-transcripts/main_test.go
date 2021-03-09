d389b04baf5aa8b78c8eceb1b9e539a3ee757732
Martin <martinald467@gmail.com>
2021-03-09 20:30:54 +0100

    add tests for the help and version flags in false case

--- cmd/yt-transcripts/main_test.go
+++ cmd/yt-transcripts/main_test.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"flag"
 	"os"
 	"testing"
 )
 
-func TestFlags(t *testing.T) {
-	os.Args = []string{"appname", "-v", "-h"}
-	v, h := flags()
+func TestShortFlagsFalse(t *testing.T) {
+	os.Args = []string{"appname"}
+	flag.Parse()
 
-	if !*v && !*h {
-		t.Fatalf("Expected true, true, got %v, %v", *v, *h)
+	if *versionShort && *helpShort {
+		t.Fatalf("Expected false, false, got %v, %v", *versionShort, *helpShort)
+	}
+}
+
+func TestLongFlagsFalse(t *testing.T) {
+	os.Args = []string{"appname"}
+	flag.Parse()
+
+	if *versionLong && *helpLong {
+		t.Fatalf("Expected false, false, got %v, %v", *versionLong, *helpLong)
+	}
+}
+
+func TestShortFlagsTrue(t *testing.T) {
+	os.Args = []string{"appname", "-v", "-h"}
+	flag.Parse()
+
+	if !*versionShort && !*helpShort {
+		t.Fatalf("Expected true, true, got %v, %v", *versionShort, *helpShort)
+	}
+}
+
+func TestLongFlagsTrue(t *testing.T) {
+	os.Args = []string{"appname", "-version", "-help"}
+	flag.Parse()
+
+	if !*versionLong && !*helpLong {
+		t.Fatalf("Expected true, true, got %v, %v", *versionLong, *helpLong)
 	}
 }
